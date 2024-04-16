a905a163110a8e414eeed80c4100cdae5655d2b5
Антон Філіпенко <filipenkoa270@gmail.com>
2024-04-16 09:15:03 +0300

    added menu

---
+++ Program.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using static System.Console;
+using static System.Array;
+namespace Laba3
+{
+    public class Public
+    {
+
+        public static int RandomInput(out int[] linearArray, out int[][] jaggedArray)
+        {
+            Write("\nInput the length of linear array: ");
+            int lengthLinearArray = int.Parse(ReadLine());
+            Random match = new Random();
+            linearArray = new int[lengthLinearArray];
+            for (int i = 0; i < lengthLinearArray; i++)
+            {
+                linearArray[i] = match.Next(-lengthLinearArray, lengthLinearArray + 1);
+            }
+
+            Write("\nInput the length of jagged array: ");
+            int lengthJaggedArray = int.Parse(ReadLine());
+            jaggedArray = new int[lengthJaggedArray][];
+
+            int max = int.MinValue;
+            for (int i = 0; i < lengthJaggedArray; i++)
+            {
+                jaggedArray[i] = new int[match.Next(1, i + 1)];
+                for (int j = 0; j < jaggedArray[i].Length; j++)
+                {
+                    jaggedArray[i][j] = match.Next(-lengthJaggedArray, lengthJaggedArray + 1);
+                    if (jaggedArray[i][j].ToString().Length > max) max = jaggedArray[i][j].ToString().Length;
+                }
+            }
+            return max;
+        }
+        public static void UserInput(out int[] linearArray, out int[][] jaggedArray)
+        {
+            Write("\nInput the length of linear array: ");
+            int lengthLinearArray = int.Parse(ReadLine());
+            linearArray = new int[lengthLinearArray];
+            string[] data = ReadLine().Split();
+            for (int i = 0; i < lengthLinearArray; i++)
+            {
+                linearArray[i] = int.Parse(data[i]);
+            }
+
+            Write("\nInput the length of jagged array: ");
+            int lengthJaggedArray = int.Parse(ReadLine());
+            jaggedArray = new int[lengthJaggedArray][];
+            WriteLine("\nInput the jagged array");
+            for (int i = 0; i < lengthJaggedArray; i++)
+            {
+                jaggedArray[i] = ConvertAll(ReadLine().Split(), int.Parse);
+            }
+        }
+
+        public static void PrintJaggedArray(int[][] jaggedArray, int max)
+        {
+
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+
+                for (int j = 0; j < jaggedArray[i].Length; j++)
+                {
+                    Write(jaggedArray[i][j].ToString().PadLeft(max + 1));
+                }
+                WriteLine();
+            }
+        }
+        public static void Main()
+        {
+            int[] linearArray;
+            int[][] jaggedArray;
+
+            WriteLine("To start working, please select the type of filling (random or user):");
+            string filling = ReadLine();
+            switch (filling)
+            {
+                case "random":
+                    int max = RandomInput(out linearArray, out jaggedArray);
+                    WriteLine("\nLinear array");
+                    WriteLine(string.Join(" ", linearArray));
+
+                    WriteLine("\nJaggedArray");
+                    PrintJaggedArray(jaggedArray, max);
+                    break;
+                case "user":
+                    UserInput(out linearArray, out jaggedArray);
+                    break;
+
+
+            }
+            string student;
+            do
+            {
+                Write("\nNow input the name of student or 'end': ");
+                student = ReadLine();
+                string variant;
+                switch (student)
+                {
+                    case "Anton":
+                        Write("Choose the variant or 'end': ");
+                        variant = ReadLine();
+                        switch (variant)
+                        {
+                            case "1":
+                                //linearArray=new Task1().Main(linearArray);
+                                break;
+                            case "2":
+                                //Task2
+                                break;
+                            case "3":
+                                //jaggedArray=new Task3().Main(jaggedArray);
+                                break;
+                            case "4":
+                                //jaggedArray=new Task4().Main(jaggedArray);
+                                break;
+                            case "end":
+                                break;
+                        }
+                        break;
+
+                    case "Anna":
+                        Write("Choose the variant or 'end': ");
+                        variant = ReadLine();
+                        switch (variant)
+                        {
+                            case "1":
+                                //linearArray=new Task1().Main(linearArray);
+                                break;
+                            case "2":
+                                //Task2
+                                break;
+                            case "3":
+                                //jaggedArray=new Task3().Main(jaggedArray);
+                                break;
+                            case "4":
+                                //jaggedArray=new Task4().Main(jaggedArray);
+                                break;
+                            case "end":
+                                break;
+                        }
+                        break;
+
+                    case "Maria":
+                        Write("Choose the variant or 'end': ");
+                        variant = ReadLine();
+                        switch (variant)
+                        {
+                            case "1":
+                                //linearArray=new Task1().Main(linearArray);
+                                break;
+                            case "2":
+                                //Task2
+                                break;
+                            case "3":
+                                //jaggedArray=new Task3().Main(jaggedArray);
+                                break;
+                            case "4":
+                                //jaggedArray=new Task4().Main(jaggedArray);
+                                break;
+                            case "end":
+                                break;
+                        }
+                        break;
+
+                    case "end":
+                        break;
+                }
+            }
+            while (student != "end");
+        }
+    }
+}
