82a786410331e4b988e99bd3d3e952bff7b9bcb1
Антон Філіпенко <filipenkoa270@gmail.com>
2024-04-16 14:10:22 +0300

    added the link of my presentation to the README

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # collaborate_laba_3
+https://docs.google.com/presentation/d/12THh9uLz9k4KZzChxRr16uoUUdPowCDkAaOj0Qwy7k8/edit?usp=sharing
--- obj/Debug/net6.0/Menu.AssemblyInfo.cs
+++ obj/Debug/net6.0/Menu.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Menu")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61df8279554a7c364a83bc358db021f68e226bdc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Menu")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Menu")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
